d72d24ed4b438ea9c7e8af59d3a2f3acce28ad5f
Blaise Palombo <blaisepalombo@users.noreply.github.com>
2026-03-20 15:45:14 -0600

    removed apiconfig from gitignore so irt works on render

---
+++ js/apiConfig.js
@@ -0,0 +1,5 @@
+export const EXERCISE_API_CONFIG = {
+  baseUrl: "https://exercisedb.p.rapidapi.com/exercises",
+  rapidApiKey: "b0c63f6357msh1f4adf87e66c9cdp1d4d01jsn58bbc47bc2d0",
+  rapidApiHost: "exercisedb.p.rapidapi.com"
+};
